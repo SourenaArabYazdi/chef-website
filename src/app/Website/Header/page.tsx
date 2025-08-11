825025c6ac3fad0152b5f29ad31b56918e383af0
Soren <yazdi_sourena@yahoo.com>
2025-08-11 09:38:30 +0330

    preparing project

---
+++ src/app/Website/Header/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function HeaderPage(){
+
+
+    return(
+        <div>
+            header
+        </div>
+    )
+}
---
+++ src/app/Website/page.tsx
@@ -0,0 +1,15 @@
+import HeaderPage from "./Header/page"
+import BodyPage from "./Body/page"
+
+
+export default function MainPage(){
+
+
+
+    return(
+        <div>
+             <HeaderPage />
+             <BodyPage />
+        </div>
+    )
+}
